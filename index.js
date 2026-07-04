3f85c327e4823fe94ba646cc8ed0544186c7343f
pedrogabrielsalvinosilva30-hash <pedrogabrielsalvinosilva30@gmail.com>
2026-07-04 02:08:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Bot da Tropa da Turquia iniciado!");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`Bot ligado como ${client.user.tag}`);
+});
+
+client.login('COLE_SEU_TOKEN_AQUI');
